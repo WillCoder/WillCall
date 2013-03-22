4d43388dd1ae94b17a4c6d82dfedd05beaebfa63
Will Du <dumohan19881231@gmail.com>
2013-03-22 15:49:30 +0800

    2.3display
    display bug

--- gen/com/will/willcall/R.java
+++ gen/com/will/willcall/R.java
@@ -18,18 +18,18 @@ public final class R {
     public static final class bool {
         /** Enable automatic activity tracking
          */
-        public static final int ga_autoActivityTracking=0x7f070000;
+        public static final int ga_autoActivityTracking=0x7f060000;
         /** Enable automatic exception tracking
          */
-        public static final int ga_reportUncaughtExceptions=0x7f070001;
+        public static final int ga_reportUncaughtExceptions=0x7f060001;
     }
     public static final class color {
-        public static final int transparent_background=0x7f080000;
+        public static final int transparent_background=0x7f070000;
     }
     public static final class dimen {
-        public static final int padding_large=0x7f090002;
-        public static final int padding_medium=0x7f090001;
-        public static final int padding_small=0x7f090000;
+        public static final int padding_large=0x7f080002;
+        public static final int padding_medium=0x7f080001;
+        public static final int padding_small=0x7f080000;
     }
     public static final class drawable {
         public static final int call_contact=0x7f020000;
@@ -41,45 +41,43 @@ public final class R {
         public static final int recall_breath_green=0x7f020006;
         public static final int recall_breath_red=0x7f020007;
         public static final int recall_breath_white=0x7f020008;
+        public static final int whiterounded_half_bg=0x7f020009;
     }
     public static final class id {
-        public static final int about_version=0x7f0b0004;
-        public static final int adView=0x7f0b0000;
-        public static final int btn_recall=0x7f0b0007;
-        public static final int btn_to_left=0x7f0b000a;
-        public static final int btn_to_right=0x7f0b000b;
-        public static final int button=0x7f0b0002;
-        public static final int mcd_name_1=0x7f0b0005;
-        public static final int mcd_name_2=0x7f0b0006;
-        public static final int mcd_time=0x7f0b0009;
-        public static final int recall_breath=0x7f0b0008;
-        public static final int state_textView=0x7f0b0001;
-        public static final int test_button=0x7f0b0003;
+        public static final int about_version=0x7f0a0004;
+        public static final int adView=0x7f0a0000;
+        public static final int btn_recall=0x7f0a0007;
+        public static final int btn_to_left=0x7f0a000a;
+        public static final int btn_to_right=0x7f0a000b;
+        public static final int button=0x7f0a0002;
+        public static final int mcd_name_1=0x7f0a0005;
+        public static final int mcd_name_2=0x7f0a0006;
+        public static final int mcd_time=0x7f0a0009;
+        public static final int recall_breath=0x7f0a0008;
+        public static final int state_textView=0x7f0a0001;
+        public static final int test_button=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int missing_call_dialog=0x7f030001;
     }
     public static final class string {
-        public static final int about_copyright=0x7f060009;
-        public static final int about_info=0x7f06000a;
-        public static final int already_close=0x7f060003;
-        public static final int already_open=0x7f060002;
-        public static final int app_name=0x7f060001;
-        public static final int btn_switch=0x7f060004;
-        public static final int btn_test=0x7f060006;
+        public static final int about_copyright=0x7f050009;
+        public static final int about_info=0x7f05000a;
+        public static final int already_close=0x7f050003;
+        public static final int already_open=0x7f050002;
+        public static final int app_name=0x7f050001;
+        public static final int btn_switch=0x7f050004;
+        public static final int btn_test=0x7f050006;
         /** Replace placeholder ID with your tracking ID
          */
-        public static final int ga_trackingId=0x7f060000;
-        public static final int ringing_Time=0x7f060007;
-        public static final int second=0x7f060008;
-        public static final int teststr=0x7f060005;
+        public static final int ga_trackingId=0x7f050000;
+        public static final int ringing_Time=0x7f050007;
+        public static final int second=0x7f050008;
+        public static final int teststr=0x7f050005;
     }
     public static final class style {
-        public static final int AppTheme=0x7f0a0000;
-        public static final int dialog=0x7f0a0001;
-    }
-    public static final class xml {
-        public static final int whiterounded_half_bg=0x7f050000;
+        public static final int AppTheme=0x7f090000;
+        public static final int dialog=0x7f090001;
     }
 }
